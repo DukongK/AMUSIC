b7170dca29a0b0225c7436d509f705d5ef42f323
DukongK <sksmsenzhd@naver.com>
2024-12-03 22:33:01 +0900

    반응형중

--- js/index.js
+++ js/index.js
@@ -45,11 +45,14 @@ $(function () {
     // console.log(con2);
     let con3 = $(this).scrollTop();
     // console.log(con3);
-
     let con4 = $(this).scrollTop();
-    console.log(con4);
+    // console.log(con4);
+    let con5 = $(this).scrollTop();
+    // console.log(con5);
     let con6 = $(this).scrollTop();
     // console.log(con6);
+    let fo = $(this).scrollTop();
+    console.log(fo);
 
     // con1
     if (con1 >= 800 && con1 <= 820) {
@@ -66,7 +69,7 @@ $(function () {
       $(".c2_center img").addClass("on");
       $(".cof").addClass("on");
       $(".cog").addClass("on");
-    } else if (con6 >= 4200 && con6 <= 4300) {
+    } else if (con6 >= 5600 && con6 <= 5700) {
       $(".txt1 h2").addClass("on");
       $(".txt2 h2").addClass("on");
       $(".txt1").addClass("on");
@@ -78,6 +81,19 @@ $(function () {
     } else if (con4 >= 2610 && con4 <= 2700) {
       $(".c4_txt h2:first-child").addClass("on");
       $(".c4_txt h2:last-child").addClass("on");
+    } else if (con5 >= 5140 && con5 <= 5200) {
+      $(".con5 ul li .c5_p1").addClass("on");
+      $(".con5 ul li:first-child ").addClass("on");
+      $(".con5 ul li img").addClass("on");
+      $(".con5 .c5_txt .p2").addClass("on");
+      $(" .con5 .c5_txt h2").addClass("on");
+      $(" .con5 .c5_txt .p3 ").addClass("on");
+      $(".con5 li:nth-child(2)").addClass("on");
+    } else if (fo >= 6200 && fo <= 6300) {
+      $(" footer > img").addClass("on");
+      $(" .footer_txt li:first-child h2 ").addClass("on");
+      $(" .footer_txt li:last-child h2 ").addClass("on");
+      $(".snsicon ").addClass("on");
     }
   });
 });
@@ -85,4 +101,6 @@ $(function () {
 // con2 1600
 // con3 2320
 // con4 2610
+// con5 5140
 // con6 4200
+// footer 6200
