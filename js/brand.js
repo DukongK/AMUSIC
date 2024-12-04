980a161133c2023105c0d02da6aeaae55882c395
DukongK <sksmsenzhd@naver.com>
2024-12-04 23:23:42 +0900

    브랜드완료

--- js/brand.js
+++ js/brand.js
@@ -3,29 +3,86 @@
 // header hambergur click
 
 $(function () {
-    $(".hambergur").on("click", function () {
-        $(".hambergur li:first-child").toggleClass("on");
-        $(".hambergur li:last-child").toggleClass("on");
-    });
+  $(".hambergur").on("click", function () {
+    $(".hambergur li:first-child").toggleClass("on");
+    $(".hambergur li:last-child").toggleClass("on");
+  });
 
-    // header hambergur mouseenter
+  // header hambergur mouseenter
 
-    $(".hambergur").on("mouseenter", function () {
-        $(".hambergur li:first-child").addClass("co");
-        $(".hambergur li:last-child").addClass("co");
-    });
-    $(".hambergur").on("mouseleave", function () {
-        $(".hambergur li:first-child").removeClass("co");
-        $(".hambergur li:last-child").removeClass("co");
-    });
+  $(".hambergur").on("mouseenter", function () {
+    $(".hambergur li:first-child").addClass("co");
+    $(".hambergur li:last-child").addClass("co");
+  });
+  $(".hambergur").on("mouseleave", function () {
+    $(".hambergur li:first-child").removeClass("co");
+    $(".hambergur li:last-child").removeClass("co");
+  });
 
-    //top button
+  //top button
 
-    $(".scrollUp").on("click", function () {
-        let button = $("header").offset().top;
-        // console.log(button);
-        $("html,body").stop().animate({ scrollTop: button });
-    });
+  $(".scrollUp").on("click", function () {
+    let button = $("header").offset().top;
+    // console.log(button);
+    $("html,body").stop().animate({ scrollTop: button });
+  });
 });
 
 // 페이지별 공통 적용------------------------------
+
+// visual
+$(function () {
+  $(" .v_txt_wrap h2:first-child ").addClass("on");
+  $(" .v_txt_wrap2 h2:first-child ").addClass("on");
+  $(" .v_txt_wrap2 ").addClass("on");
+  $(" .txt1 ").addClass("on");
+});
+
+$(function () {
+  $(window).on("scroll", function () {
+    let con1 = $(this).scrollTop();
+    // console.log(con1);
+    let con2 = $(this).scrollTop();
+    // console.log(con2);
+    let con3 = $(this).scrollTop();
+    // console.log(con3);
+    let con5 = $(this).scrollTop();
+    // console.log(con5);
+    let fo = $(this).scrollTop();
+    console.log(fo);
+
+    if (con1 >= 530 && con1 <= 600) {
+      $(".img1").addClass("on");
+    } else if (con1 >= 980 && con1 <= 1100) {
+      $(".img2").addClass("on");
+    } else if (con1 >= 1410 && con1 <= 1500) {
+      $(".img3").addClass("on");
+    } else if (con2 >= 1670 && con2 <= 1750) {
+      $(".con2_bg").addClass("on");
+    } else if (con2 >= 2150 && con2 <= 2250) {
+      $(".con2_txt h2:first-child").addClass("on");
+      $(".con2_txt h2:last-child").addClass("on");
+    } else if (con3 >= 2700 && con3 <= 2800) {
+      $(" .con3_ul li:nth-child(2)").addClass("on");
+      $(".con3_ul li:nth-child(1)").addClass("on");
+      $(".con3_ul li:nth-child(3)").addClass("on");
+      $(".color1").addClass("on");
+      $(".color2").addClass("on");
+    } else if (con5 >= 3910 && con5 <= 4100) {
+      $(".con5 ul").addClass("on");
+      $(".c5_txt1").addClass("on");
+      $(".c5_txt2").addClass("on");
+      $(" .con5 li:first-child ").addClass("on");
+    } else if (fo >= 4200 && fo <= 4500) {
+      $(" footer > img").addClass("on");
+      $(" .footer_txt li:first-child h2 ").addClass("on");
+      $(" .footer_txt li:last-child h2 ").addClass("on");
+      $(".snsicon ").addClass("on");
+    }
+  });
+});
+
+// con1 530 , 980 , 1410
+// con2 1670 , 2150
+// con3 2700
+// con5 3910
--- js/index.js
+++ js/index.js
@@ -3,101 +3,101 @@
 // header hambergur click
 
 $(function () {
-    $(".hambergur").on("click", function () {
-        $(".hambergur li:first-child").toggleClass("on");
-        $(".hambergur li:last-child").toggleClass("on");
-    });
+  $(".hambergur").on("click", function () {
+    $(".hambergur li:first-child").toggleClass("on");
+    $(".hambergur li:last-child").toggleClass("on");
+  });
 
-    // header hambergur mouseenter
+  // header hambergur mouseenter
 
-    $(".hambergur").on("mouseenter", function () {
-        $(".hambergur li:first-child").addClass("co");
-        $(".hambergur li:last-child").addClass("co");
-    });
-    $(".hambergur").on("mouseleave", function () {
-        $(".hambergur li:first-child").removeClass("co");
-        $(".hambergur li:last-child").removeClass("co");
-    });
+  $(".hambergur").on("mouseenter", function () {
+    $(".hambergur li:first-child").addClass("co");
+    $(".hambergur li:last-child").addClass("co");
+  });
+  $(".hambergur").on("mouseleave", function () {
+    $(".hambergur li:first-child").removeClass("co");
+    $(".hambergur li:last-child").removeClass("co");
+  });
 
-    //top button
+  //top button
 
-    $(".scrollUp").on("click", function () {
-        let button = $("header").offset().top;
-        // console.log(button);
-        $("html,body").stop().animate({ scrollTop: button });
-    });
+  $(".scrollUp").on("click", function () {
+    let button = $("header").offset().top;
+    // console.log(button);
+    $("html,body").stop().animate({ scrollTop: button });
+  });
 });
 
 // 페이지별 공통 적용------------------------------
 
 // visual 첫 화면
 $(function () {
-    $(".top_txt").addClass("on");
-    $(".zin2").addClass("on");
-    $(".center_txt img").addClass("on");
-    $(".center_line").addClass("on");
-    $(".banner").addClass("on");
+  $(".top_txt").addClass("on");
+  $(".zin2").addClass("on");
+  $(".center_txt img").addClass("on");
+  $(".center_line").addClass("on");
+  $(".banner").addClass("on");
 });
 
 $(function () {
-    $(window).on("scroll", function () {
-        let con1 = $(this).scrollTop();
-        // console.log(con1);
-        let con2 = $(this).scrollTop();
-        // console.log(con2);
-        let con3 = $(this).scrollTop();
-        // console.log(con3);
-        let con4 = $(this).scrollTop();
-        // console.log(con4);
-        let con5 = $(this).scrollTop();
-        // console.log(con5);
-        let con6 = $(this).scrollTop();
-        // console.log(con6);
-        let fo = $(this).scrollTop();
-        console.log(fo);
+  $(window).on("scroll", function () {
+    let con1 = $(this).scrollTop();
+    // console.log(con1);
+    let con2 = $(this).scrollTop();
+    // console.log(con2);
+    let con3 = $(this).scrollTop();
+    // console.log(con3);
+    let con4 = $(this).scrollTop();
+    // console.log(con4);
+    let con5 = $(this).scrollTop();
+    // console.log(con5);
+    let con6 = $(this).scrollTop();
+    // console.log(con6);
+    let fo = $(this).scrollTop();
+    console.log(fo);
 
-        // con1
-        if (con1 >= 800 && con1 <= 820) {
-            $(".img_txt").addClass("on");
-        } else if (con2 >= 1600 && con2 <= 1700) {
-            $(".ring_left div:nth-child(3)").addClass("on");
-            $(".ring_left div:nth-child(2)").addClass("on");
-            $(".ring_left div:nth-child(1)").addClass("on");
-            $(".ring_left div:nth-child(4)").addClass("on");
-            $(".ring_right div:nth-child(3)").addClass("on");
-            $(".ring_right div:nth-child(2)").addClass("on");
-            $(".ring_right div:nth-child(1)").addClass("on");
-            $(".ring_right div:nth-child(4)").addClass("on");
-            $(".c2_center img").addClass("on");
-            $(".cof").addClass("on");
-            $(".cog").addClass("on");
-        } else if (con6 >= 5600 && con6 <= 5700) {
-            $(".txt1 h2").addClass("on");
-            $(".txt2 h2").addClass("on");
-            $(".txt1").addClass("on");
-            $(".txt2").addClass("on");
-            $(".txt3 h2").addClass("on");
-        } else if (con3 >= 2320 && con3 <= 2400) {
-            $(".line1").addClass("on");
-            $(".line2").addClass("on");
-        } else if (con4 >= 2610 && con4 <= 2700) {
-            $(".c4_txt h2:first-child").addClass("on");
-            $(".c4_txt h2:last-child").addClass("on");
-        } else if (con5 >= 5140 && con5 <= 5200) {
-            $(".con5 ul li .c5_p1").addClass("on");
-            $(".con5 ul li:first-child ").addClass("on");
-            $(".con5 ul li img").addClass("on");
-            $(".con5 .c5_txt .p2").addClass("on");
-            $(" .con5 .c5_txt h2").addClass("on");
-            $(" .con5 .c5_txt .p3 ").addClass("on");
-            $(".con5 li:nth-child(2)").addClass("on");
-        } else if (fo >= 6200 && fo <= 6300) {
-            $(" footer > img").addClass("on");
-            $(" .footer_txt li:first-child h2 ").addClass("on");
-            $(" .footer_txt li:last-child h2 ").addClass("on");
-            $(".snsicon ").addClass("on");
-        }
-    });
+    // con1
+    if (con1 >= 800 && con1 <= 820) {
+      $(".img_txt").addClass("on");
+    } else if (con2 >= 1600 && con2 <= 1700) {
+      $(".ring_left div:nth-child(3)").addClass("on");
+      $(".ring_left div:nth-child(2)").addClass("on");
+      $(".ring_left div:nth-child(1)").addClass("on");
+      $(".ring_left div:nth-child(4)").addClass("on");
+      $(".ring_right div:nth-child(3)").addClass("on");
+      $(".ring_right div:nth-child(2)").addClass("on");
+      $(".ring_right div:nth-child(1)").addClass("on");
+      $(".ring_right div:nth-child(4)").addClass("on");
+      $(".c2_center img").addClass("on");
+      $(".cof").addClass("on");
+      $(".cog").addClass("on");
+    } else if (con6 >= 5600 && con6 <= 5700) {
+      $(".txt1 h2").addClass("on");
+      $(".txt2 h2").addClass("on");
+      $(".txt1").addClass("on");
+      $(".txt2").addClass("on");
+      $(".txt3 h2").addClass("on");
+    } else if (con3 >= 2320 && con3 <= 2400) {
+      $(".line1").addClass("on");
+      $(".line2").addClass("on");
+    } else if (con4 >= 2610 && con4 <= 2700) {
+      $(".c4_txt h2:first-child").addClass("on");
+      $(".c4_txt h2:last-child").addClass("on");
+    } else if (con5 >= 5140 && con5 <= 5200) {
+      $(".con5 ul li .c5_p1").addClass("on");
+      $(".con5 ul li:first-child ").addClass("on");
+      $(".con5 ul li img").addClass("on");
+      $(".con5 .c5_txt .p2").addClass("on");
+      $(" .con5 .c5_txt h2").addClass("on");
+      $(" .con5 .c5_txt .p3 ").addClass("on");
+      $(".con5 li:nth-child(2)").addClass("on");
+    } else if (fo >= 6000 && fo <= 6300) {
+      $(" footer > img").addClass("on");
+      $(" .footer_txt li:first-child h2 ").addClass("on");
+      $(" .footer_txt li:last-child h2 ").addClass("on");
+      $(".snsicon ").addClass("on");
+    }
+  });
 });
 
 // con2 1600
