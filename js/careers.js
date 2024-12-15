ff2e2f43661f37b070fb31de8c0aa76025d3c581
DukongK <sksmsenzhd@naver.com>
2024-12-15 16:47:36 +0900

    전체수정

--- js/careers.js
+++ js/careers.js
@@ -29,10 +29,10 @@ $(function () {
 
   // hamlist
 
-$(".hambergur").on("click",function(){
-  $(".hamlist").fadeToggle();
-  $(".ham_list_ul li").toggleClass("on");
-})
+  $(".hambergur").on("click", function () {
+    $(".hamlist").fadeToggle();
+    $(".ham_list_ul li").toggleClass("on");
+  });
 });
 
 // 페이지별 공통 적용------------------------------
@@ -61,6 +61,7 @@ $(function () {
       $(".con1 h2 ").addClass("on");
       $(".cog ").addClass("on");
     }
+
     if (con2 >= 1360 && 1450 <= con2) {
       $(".con2 ul:first-child li h2:last-child").addClass("on");
       $(".con2 ul:first-child li h2:first-child").addClass("on");
